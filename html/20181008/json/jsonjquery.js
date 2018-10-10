0d32610b971ea9410290fe69e6ddde37a36864b2
unknown <ch4ut1nhtr1@gmail.com>
2018-10-10 17:08:38 +0700

    updated

---
+++ html/20181008/json/jsonjquery.js
@@ -0,0 +1,17 @@
+var table = '<table id="js-table" class="table"></table>';
+    $('#js-div-table').append(table);
+    var thead = '<thead id="js-thead" class="thead bg-success"></thead>';
+    $(thead).appendTo('#js-table');
+    var tr = '<tr id="js-tr-head"></tr>';
+    $(tr).appendTo('#js-thead');
+    var th = '<th>Id</th><th>Name</th><th>Email</th><th>Password</th><th>Created_at</th><th>Updated_at</th>';
+    $(th).appendTo('#js-tr-head');
+    var tbody = ' <tbody class="tbody bg-info"></tbody>';
+    $('#js-thead').after(tbody);
+    for (i = 0; i < a.users.length - 1; i++) {
+      var trBody = '<tr id="trBody-' + a.users[i].id + '"></tr>';
+      $(trBody).appendTo('tbody');
+      var thBody = ' <th>' + (i + 1) + '</th>';
+      var tdBody = '<td>' + a.users[i].name + '</td><td>' + a.users[i].email + '</td><td>'+ a.users[i].password + '</td><td>'+ a.users[i].created_at + '</td><td>'+ a.users[i].updated_at + '</td> ';
+      $('#trBody-' + a.users[i].id).append(thBody, tdBody);
+    }
