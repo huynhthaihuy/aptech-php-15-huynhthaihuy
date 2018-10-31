a5aa75e729d8266aa251de42ada477334b349182
unknown <ch4ut1nhtr1@gmail.com>
2018-10-31 15:00:53 +0700

    updated

--- php/26-10-2018/Selectpost.php
+++ php/26-10-2018/Selectpost.php
@@ -1,30 +1,22 @@
 <?php
 if($_POST['email'] === 'kinsatthu123@yahoo.com' & $_POST['password'] === '123456'){
-$servername='localhost';
-$user='root';
-$password='';
-$db = 'aptech_php_15_ss3';
-// Create connection
-$conn = new mysqli($servername, $user, $password,$db);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . mysqli_connect_error());
-} 
-$sql = "SELECT ID, Title, Category_id FROM posts";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["ID"]. " - Title: " . $row["Title"]. " -categories_id " . $row["Category_id"]. "<br>";
+    include('connect.php');
+    $sql = "SELECT id,name,email,password FROM users";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        echo "<table border='1'><tr><th>ID</th><th>Name</th><th>Email</th><th>PassWord</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td> ".$row["email"]."</td><td>".$row["password"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
     }
-} else {
-    echo "0 results";
-}
-$conn->close();
+    $conn->close();
 }
 else{
-    header('Location: ./FormPOST.php');
+    header('Location: ./FormSelectPOST.php');
 }
 ?>
