e6c51cd678c61e1121c26cdc43644bc88cd67d2e
unknown <ch4ut1nhtr1@gmail.com>
2018-11-02 20:30:47 +0700

    updated

--- php/02-11-2018/route-view/routes/web.php
+++ php/02-11-2018/route-view/routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('hello',function(){
+Route::get('/huyhuynh', function () {
     return view('huyhuynh');
 });
+Route::get('/index','Controller@index');
